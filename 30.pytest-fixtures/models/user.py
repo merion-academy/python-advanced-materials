9c6aa117bbb2af561e998c7b3cac4e4261eb4575
Suren Khorenyan <surenkhorenyan@gmail.com>
2023-09-03 16:02:57 +0300

    add lesson 30: pytest fixtures

---
+++ 30.pytest-fixtures/models/user.py
@@ -0,0 +1,31 @@
+import hashlib
+
+
+def hash_password(value: str):
+    return hashlib.sha256(value.encode()).hexdigest()
+
+
+def password_validator(pwd, hashed):
+    hashed_pwd = hash_password(pwd)
+    return hashed_pwd == hashed
+
+
+class User:
+    def __init__(self, username, password):
+        self.username = username
+        self.__password = None
+        self.password = password
+
+    @property
+    def password(self):
+        return self.__password
+
+    @password.setter
+    def password(self, value):
+        if value is not None:
+            value = hash_password(value)
+        self.__password = value
+
+    @password.deleter
+    def password(self):
+        self.__password = None
